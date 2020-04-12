1d3d1b687ef5c77c05737fd3d7b9a3ea74fffbe6
Gábor Kriskó <gabor.krisko@gmail.com>
2020-04-12 15:37:25 +0200

    syntax highlighting

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 /* let math = "7.56 * 28 / 3 + 6.5 * 56 / 456 - 446 + 654"; */
-let math = "((5*5) - 20)! * 2";
+let math = "((5*5)-20)!*2";
 /* let math = "5 * 10 + 8 / 5 - 16" */
 /* let math = "0 + sin(1)" */
 
 let input = document.getElementById("input");
+let syntax = document.getElementById("syntax");
 
 input.oninput = function(){
     onInputChange();
@@ -13,10 +14,29 @@ let output = document.getElementById("output");
 input.value = math;
 
 function onInputChange(){
+    let math = input.value;
 
-    console.log("running",input.value);
+    let tokens = tokenize(math);
+    
+    /* console.log(tokens); */
+    tokens = tokens.map((token)=>{
+        return token.string.split("").map((char, index)=>{
+            let t = new token.constructor();
+            console.log(token.foundAt,index,char);
+            t.string = char;
+            t.foundAt = token.foundAt+index;
+            return t;
+        })
+    }).flat();
+
+    let isBrackets = (t)=>t.string == "(" || t.string == ")";
+
+    syntax.innerHTML = tokens.map((token)=>{
+        return `<span class="${token.constructor.name} ${isBrackets(token) ? 'brackets' : ''}">${token.string}</span>`
+    }).join("");
+    console.log("running",math);
     try {
-        output.innerHTML = evalMath(input.value);
+        output.innerHTML = evalMath(math,{});
     } catch(err){
         output.innerHTML = "Error";
     }
